30a053865f1b6400413e67238196e93d05b4f4fd
kkulhavi <78230187+kkulhavi@users.noreply.github.com>
2021-10-29 11:05:21 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const Sequelize=require('sequelize')
 let env=process.env.NODE_ENV || "development"
 //const config=require('./config/config.json')["production"]
 //console.log(config)
-let dbConnection = new Sequelize('mysql://eq5s24k4itiufgwg:vb3x2eeod49ci17u@l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zzqynl7chv706qns','eq5s24k4itiufgwg','vb3x2eeod49ci17u',{
+let dbConnection = new Sequelize('zzqynl7chv706qns','eq5s24k4itiufgwg','vb3x2eeod49ci17u',{
     dialect:'mysql',
     host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
     });

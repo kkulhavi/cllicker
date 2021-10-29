3ad462ed6eef3620fcd30e602f0506318e0baed1
kkulhavi <78230187+kkulhavi@users.noreply.github.com>
2021-10-29 18:52:19 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,8 @@ const Sequelize=require('sequelize')
 let env=process.env.NODE_ENV || "development"
 //const config=require('./config/config.json')["production"]
 //console.log(config)
-let dbConnection = new Sequelize('heroku_45a657f0cf29b9f','bcb8dea161b467','e00a35a9',{
+//let dbConnection = new Sequelize('heroku_45a657f0cf29b9f','bcb8dea161b467','e00a35a9',{
+let dbConnection = new Sequelize('heroku_45a657f0cf29b9f','bcb8dea161b467',process.env.PASS,{
     dialect:'mysql',
     host: 'us-cdbr-east-04.cleardb.com'
     });
